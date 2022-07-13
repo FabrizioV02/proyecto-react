9255782536997d3d75ef367a54a3b8a6885f6213
Fabrizio Valdovinos <fabriziovaldovinos162@gmail.com>
2022-07-13 18:19:48 -0300

    ajustes visuales

--- src/containers/ItemListContainer/Item.jsx
+++ src/containers/ItemListContainer/Item.jsx
@@ -3,31 +3,23 @@ import { Link } from "react-router-dom";
 
 const Item = ({ producto }) => {
   return (
-    <div>
-      <div className="card">
-        <div className="header-wrapper">
-          <div className="image-wrapper">
-            <img src={producto.image} alt="" />
-          </div>
-        </div>
-        <div className="text-wrapper">
-          <div className="title-wrapper">
-            <h2>{producto.title}</h2>
-          </div>
-          <div className="price-wrapper">
-            <p>${producto.price}</p>
-          </div>
-          <div className="id-wrapper">
-            <div key={producto.id}>
-              <Link to={`/item/${producto.id}`}>ver mas</Link>
+    <div className="wrapper flex items-center justify-center min-h-screen container-mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="card">
+          <div className="p-5 flex flex-col">
+            <div className="rounded-xl overflow-hidden">
+              <img src={producto.image} alt="" />
+            </div>
+            <h2 className="text-2xl md:text-3xl font-medium mt-3 text-center">{producto.title}</h2>
+            <p className=" text-center text-black text-lg mt-3">${producto.price}</p>
+            <div className="text-center bg-black text-white py-2 rounded-lg font-semibold mt-4 hover:bg-red-700 focus:scale-95 transition-all duration-200 ease-out" key={producto.id}>
+              <Link  to={`/item/${producto.id}`}
+              >VER MAS</Link>
             </div>
           </div>
-          <div className="description-wrapper">
-            <p>{producto.description}</p>
-          </div>
+        </div> 
         </div>
       </div>
-    </div>
   );
 };
 
