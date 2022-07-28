b9e94cd5c0bb9f8591b2a8330f9a45508cd8a9f0
Fabrizio <fabriziovaldovinos162@gmail.com>
2022-07-27 23:14:51 -0300

    item collection 2

--- src/components/Cart/cart.jsx
+++ src/components/Cart/cart.jsx
@@ -1,9 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { cartContext } from "../../Context/CartContext";
 import { Link } from "react-router-dom";
+import { db } from "../../firebase/firebase";
+import {doc, addDoc, collection, serverTimestamp} from 'firebase/firestore'
 
 const Cart = () => {
   const { products, clear, deleteProduct, totalCost } = useContext(cartContext);
+
+  const [ saleId, setSaleId] = useState("")
+
+  const datosComprador = {
+    nombre: 'Enrique',
+    apellido: 'Lopez',
+    email: 'juanlopez@gmail.com'
+  }
+  
+  const finalizarCompra = () => {
+    const ventasCollection = collection (db, 'ventas');
+    addDoc(ventasCollection,{
+      datosComprador,
+      items: [{nombre: 'remera', id: 1},{nombre: 'saco', id: 2}],
+      date: serverTimestamp(),
+      total:500,
+
+    })
+    .then((resultado) => {
+      setSaleId(resultado.id);
+  })
+  }
+
  
   if (products.length === 0) {
     return (
@@ -44,6 +69,9 @@ const Cart = () => {
     </div>
     <div>
         <p className="text-center">${totalCost()}</p>
+    </div>
+    <div className="flex">
+        <button onClick={finalizarCompra} className="uppercase mx-auto text-xl text-center bg-green-700 text-white py-2 rounded-lg font-semibold mt-4 hover:bg-green-500 focus:scale-95 transition-all duration-200 ease-out px-20 my-5">Comprar</button>
     </div>
     <div className="flex">
         <button onClick={clear} className="uppercase mx-auto text-xl text-center bg-red-700 text-white py-2 rounded-lg font-semibold mt-4 hover:bg-red-500 focus:scale-95 transition-all duration-200 ease-out px-20 my-5">Vaciar Carrito</button>
